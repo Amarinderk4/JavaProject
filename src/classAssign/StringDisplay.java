c27e39e887a6338113fb59f907cfb6750721e02e
Amarinderkaur <Amarinderk4@gmail.com>
2023-07-05 19:07:08 -0400

    New package added

---
+++ src/classAssign/StringDisplay.java
@@ -0,0 +1,12 @@
+package classAssign;
+
+public class StringDisplay {
+	
+	String name;
+	
+	void DisplayName() {
+		
+		System.out.println("Hello welcome to the class");
+	}
+
+}
---
+++ src/classAssign/StringMain.java
@@ -0,0 +1,12 @@
+package classAssign;
+
+public class StringMain {
+
+	public static void main(String[] args) {
+		
+		StringDisplay sd= new StringDisplay();
+		sd.DisplayName();
+
+	}
+
+}
