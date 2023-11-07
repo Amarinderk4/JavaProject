6d5c3ba56312aebe3d0021f572bd293b5cb2c4aa
Amarinderkaur <Amarinderk4@gmail.com>
2023-11-07 16:30:53 -0500

    prgram

---
+++ src/condStatment/PrintNumbers.java
@@ -0,0 +1,34 @@
+package condStatment;
+
+public class PrintNumbers {
+
+	int number;
+	int finalNumber;
+	
+	void printSuccNumbers() {
+		while(number <= finalNumber) {
+			if(number!=13) {
+		System.out.println(number);
+				
+			number++;
+			
+			}
+		}
+	}
+}
+		
+			
+			 
+		
+		
+		//void printReverseNumbers() {
+//			while(number<=finalNumber) {
+//				System.out.println(finalNumber);
+//			}
+//			finalNumber--;
+//			
+//		}}
+	
+	
+	
+
